6ff0d335901c2984ab0af4ebabb11874290618e9
Nazar <nazary9911@gmail.com>
2026-03-18 09:03:25 +0200

    Chore: Removed unused /generation page route

--- app/generation/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Generation() {
-  return (
-    <div className="h-[70vh] flex justify-center items-center">
-      <h1 className="text-3xl text-mainText dark:text-white">Automatic generation ON</h1>
-    </div>
-  );
-}
