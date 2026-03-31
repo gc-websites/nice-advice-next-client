40e30168fa4babf49a1490b3c60263e0e83e974d
Nazar <nazary9911@gmail.com>
2026-03-31 07:43:48 +0300

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: "nice-advice-next",
-      script: "npm",
-      args: "start",
+      name: 'nice-advice-next',
+      script: 'npm',
+      args: 'run start',
       instances: 1,
-      exec_mode: "fork"
-    }
-  ]
-}
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+  ],
+};
